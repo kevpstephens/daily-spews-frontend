f9a1a6ecb854301aba190969e6688f401a9609ac
kevpstephens <kevpstephenson@gmail.com>
2025-05-19 10:28:55 +0100

    1-CORE: Create a React project and a public repo - Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <h1>NC News Frontend</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
