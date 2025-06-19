f3fd0a4d0a853fe360fa9432a8e0b18df89f5765
kevpstephens <kevpstephenson@gmail.com>
2025-06-19 23:46:22 +0100

    Refactor: ArticleCard Layout & Improve Styling Consistency

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -7,18 +7,25 @@ export default function ArticleCard({ article }) {
   return (
     <>
       <div className="article-card-container">
-        <Link
-          to={`/articles/${article.article_id}`}
-          className="article-card-link"
-        >
-          <article className="article-card">
-            <h2 className="article-card-heading">{article.title}</h2>
-            <p>
-              Written by:{" "}
-              <Link to={`/users/${article.author}`}>{article.author}</Link> |
-              Topic:{" "}
-              <Link to={`/topics/${article.topic}`}>{`#${article.topic}`}</Link>
-            </p>
+        <article className="article-card">
+          <h2 className="article-card-heading">{article.title}</h2>
+          <p>
+            Written by:{" "}
+            <Link
+              className="article-card-author-and-topic-link"
+              to={`/users/${article.author}`}
+            >
+              {article.author}
+            </Link>{" "}
+            | Topic:{" "}
+            <Link
+              className="article-card-author-and-topic-link"
+              to={`/topics/${article.topic}`}
+            >
+              {`#${article.topic}`}
+            </Link>
+          </p>
+          <Link to={`/articles/${article.article_id}`}>
             <img
               src={
                 article.article_img_url ||
@@ -26,20 +33,20 @@ export default function ArticleCard({ article }) {
               }
               alt={`${article.title} image`}
             />
-            <p>Posted: {formatDate(article.created_at)}</p>
-          </article>
-        </Link>
+          </Link>
+          <p>Posted: {formatDate(article.created_at)}</p>
+        </article>
         <div className="article-card-likes-and-comments-container">
           <Link
-            className="article-card-likes-link"
+            className="article-card-likes-icon-link"
             to={`/articles/${article.article_id}#comments`}
           >
-            <div className="article-card-likes-and-comments-container-item">
-              <MessageSquareMore className="article-card-message-square-icon" />
+            <div className="article-card-likes-and-comments-item">
+              <MessageSquareMore className="article-card-message-icon" />
               {article.comment_count}
             </div>
           </Link>
-          <div className="article-card-likes-and-comments-container-item">
+          <div className="article-card-likes-and-comments-item">
             <Heart className="article-card-heart-icon" /> {article.votes}
           </div>
         </div>
